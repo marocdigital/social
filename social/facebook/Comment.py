1dd6ca1b390ec56f149471467f9bfecce84ef309
IbtissamBerroho <118131299+IbtissamBerroho@users.noreply.github.com>
2023-05-15 15:16:59 +0100

    Update Comment.py

--- social/facebook/Comment.py
+++ social/facebook/Comment.py
@@ -4,8 +4,8 @@
 #This includes the comment's content, date, time, author, and engagement metrics (likes, replies, etc.).
 import requests
 import facebook
-id = '109105195512089'
-access_token = 'EAALGGR827TQBAMGCv7KaTgwJFJ7ApQfbaFYhrfGJblcjv5iUug5QxwVG1A7MCr6OsN1F2uRORf61jMKeqcMYMbaIkollZCYMoxv9nNjxFhkxWfrSsW4zeDX6JCFvQasd2HbdZAd53b9oDwzxKPjGutFdmOs8BM1EdXGjIcuFiH0M83RWKXJy2FkHO4UokZBxgFKuZAWPvEzSLK06Jwh9'
+id = 'page_id'
+access_token = 'access_token'
 graph = facebook.GraphAPI(access_token)
 posts = graph.get_object(id=id, fields='posts')
 Posts = posts['posts']['data']
