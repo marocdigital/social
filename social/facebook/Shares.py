475dd73d6a08a1fbedf0ccfb4a2051b5df363496
IbtissamBerroho <118131299+IbtissamBerroho@users.noreply.github.com>
2023-05-15 15:19:15 +0100

    Update Shares.py

--- social/facebook/Shares.py
+++ social/facebook/Shares.py
@@ -1,6 +1,6 @@
 import facebook
-id = '109105195512089'
-access_token = 'EAALGGR827TQBAMGCv7KaTgwJFJ7ApQfbaFYhrfGJblcjv5iUug5QxwVG1A7MCr6OsN1F2uRORf61jMKeqcMYMbaIkollZCYMoxv9nNjxFhkxWfrSsW4zeDX6JCFvQasd2HbdZAd53b9oDwzxKPjGutFdmOs8BM1EdXGjIcuFiH0M83RWKXJy2FkHO4UokZBxgFKuZAWPvEzSLK06Jwh9'
+id = 'page_id'
+access_token = 'access_token'
 graph = facebook.GraphAPI(access_token)
 Shares = graph.get_object(id=id, fields='posts{shares}')
 postsShares = Shares['posts']['data']
